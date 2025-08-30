f03b301f9fb210171263b48074319a31c7c412f2
dvlprasher5 <jr.asher72@gmail.com>
2025-08-30 22:50:22 +0500

    Fix formatting in CONTACT_LINKS by removing unnecessary line breaks

--- lib/constants.ts
+++ lib/constants.ts
@@ -13,12 +13,11 @@ export const CONTACT_LINKS: NavItem[] = [
     label: '37°47\'33.4"N 122°24\'18.6"W',
     href: 'https://maps.app.goo.gl/SLr5HYJKua6zKcZg7',
   },
-
+  
   {
     label: 'Github',
     href: 'https://github.com/dvlprasher5',
   },
-  
   {
     label: 'Instagram',
     href: 'https://www.instagram.com/yru_waitingasheryy/',
